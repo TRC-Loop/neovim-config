f2486920d4703c4cd63d129bab5f820d419f2f0e
TRC Loop <ak@stellar-code.com>
2025-06-08 16:43:16 +0200

    Setup Lazy

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
+

6de7b30dcf153576a9bd290e4ef3d1ed85e8ec46
TRC Loop <ak@stellar-code.com>
2025-06-08 16:48:46 +0200

    Added Neotree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,2 +1,3 @@
 return {
+  require("plugins.neo-tree"),
 }

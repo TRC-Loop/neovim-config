329d7e74e03c3421bcbe66fa13c9240a654ac895
TRC Loop <ak@stellar-code.com>
2025-06-09 12:50:47 +0200

    Update Git Blame to show more Info

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -1,9 +1,9 @@
 return {
   "f-person/git-blame.nvim",
+  event = "VeryLazy",
   config = function()
     vim.g.gitblame_enabled = 1
-    vim.g.gitblame_message_template = '<author>, <date>'
+    vim.g.gitblame_message_template = " <summary> • <date> • <author> • <<sha>>"
     vim.g.gitblame_date_format = '%Y-%m-%d'
   end,
-  event = "BufReadPost",
 }

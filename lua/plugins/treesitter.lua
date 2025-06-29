cee0c24a0d48206d3a656e261daae8c3a6fbebde
TRC Loop <ak@stellar-code.com>
2025-06-29 13:30:07 +0200

    Add nvim-ts-autotag to Treesitter plugins
    Integrated the 'nvim-ts-autotag' plugin as a dependency for nvim-treesitter and enabled its autotag feature in the configuration. This enhances automatic tag management for supported filetypes.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,11 +2,17 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'windwp/nvim-ts-autotag',
+    },
     config = function()
       require('nvim-treesitter.configs').setup {
         ensure_installed = { 'lua', 'python', 'json', 'html', 'bash' },
         highlight = { enable = true },
         indent = { enable = true },
+        autotag = {
+          enable = true,
+        },
       }
     end
   }

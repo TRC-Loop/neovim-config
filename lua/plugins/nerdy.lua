4d250d3d3c396a33e2032ae8855bd974087aec18
TRC Loop <ak@stellar-code.com>
2025-07-22 13:23:49 +0200

    Add Nerdfonts Icon Picker (Nerdy)

---
+++ lua/plugins/nerdy.lua
@@ -0,0 +1,12 @@
+return {
+    '2kabhishek/nerdy.nvim',
+    dependencies = {
+        'folke/snacks.nvim',
+    },
+    cmd = 'Nerdy',
+    opts = {
+        max_recents = 30,
+        add_default_keybindings = true,
+        use_new_command = true,
+    }
+}

78fc89193e31306d1be3a5191790d371135f54f3
TRC Loop <ak@stellar-code.com>
2025-10-12 13:50:05 +0200

    Added Treesitter Textobjects
    Co-Authored-By: EikDaMike <127220881+eikdamike@users.noreply.github.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,14 +4,31 @@ return {
     build = ':TSUpdate',
     dependencies = {
       'windwp/nvim-ts-autotag',
+      'nvim-treesitter/nvim-treesitter-textobjects', 
     },
     config = function()
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'lua', 'python', 'json', 'html', 'bash' },
+        ensure_installed = { 'lua', 'python', 'json', 'html', 'bash', 'php', 'javascript' },
         highlight = { enable = true },
         indent = { enable = true },
         autotag = {
           enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- auto-jump forward to textobj
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aC"] = "@conditional.outer",
+              ["iC"] = "@conditional.inner",
+              ["ap"] = "@parameter.outer",
+              ["ip"] = "@parameter.inner",
+            },
+          },
         },
       }
     end

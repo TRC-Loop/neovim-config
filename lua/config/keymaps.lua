b054441fb7ff6c9a10a85ddd15187ee50c7c4566
TRC Loop <ak@stellar-code.com>
2025-10-12 12:45:20 +0200

    Improve terminal integration and keymaps
    Enhanced ToggleTerm plugin configuration with custom terminal setup, keymaps for toggling terminals, and automatic buffer cleanup on terminal close. Updated keymaps.lua to remove duplicate terminal keymap, add Wqa command, and ensure terminal jobs are stopped before quitting Neovim.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,10 +8,9 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
 
-vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true })
-
 vim.cmd("command! Wq wq")
 vim.cmd("command! WQ wq")
+vim.cmd("command! Wqa wq")
 
 vim.keymap.set('n', '<leader>+', ':tabnew<CR>', { desc = 'New Tab' })
 vim.keymap.set('n', '<leader>-', ':tabclose<CR>', { desc = 'Close Tab' })
@@ -22,3 +21,13 @@ vim.keymap.set('n', '<leader>h', ':tabprevious<CR>', { desc = 'Previous Tab' })
 for i = 1, 9 do
   vim.keymap.set('n', '<leader>' .. i, i .. 'gt', { desc = 'Go to Tab ' .. i })
 end
+
+vim.api.nvim_create_autocmd("QuitPre", {
+    callback = function()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.bo[buf].buftype == "terminal" then
+                vim.fn.jobstop(vim.b[buf].terminal_job_id)
+            end
+        end
+    end,
+})
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,43 @@
+
 return {
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = function()
+            local toggleterm = require("toggleterm")
+            toggleterm.setup({
+                size = 20,
+                direction = "horizontal",
+                persist_size = true,
+                start_in_insert = true,
+            })
+
+            local Terminal = require('toggleterm.terminal').Terminal
+            local my_terminal = Terminal:new({
+                hidden = true,
+                close_on_exit = true,
+            })
+
+            function _G.toggle_my_terminal()
+                my_terminal:toggle()
+                if my_terminal:is_open() then
+                    vim.cmd("startinsert")
+                end
+            end
+
+            vim.api.nvim_set_keymap("n", "<C-t>", "<cmd>lua toggle_my_terminal()<CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap("t", "<C-t>", "<C-\\><C-n><cmd>lua toggle_my_terminal()<CR>", { noremap = true, silent = true })
+
+            -- Automatically delete terminal buffers on close to prevent blocking :wqa
+            vim.api.nvim_create_autocmd("TermClose", {
+                pattern = "*",
+                callback = function()
+                    local bufnr = vim.fn.expand("<abuf>")
+                    if vim.api.nvim_buf_is_valid(bufnr) then
+                        vim.api.nvim_buf_delete(bufnr, { force = true })
+                    end
+                end,
+            })
+        end
+    }
 }
